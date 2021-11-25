91b12d84c7de5d0451d8c95488468bc78e29a2f3
Kelta-King <kushangshah131@gmail.com>
2021-11-25 14:14:45 +0530

    exercise

--- trial.py
+++ trial.py
@@ -1,6 +1,33 @@
-import datetime
 import hashlib
 import json
 
-val1 = 1
-val2 = 
+def hash(block):
+    encoded_block = json.dumps(block, sort_keys = True).encode()
+    return hashlib.sha256(encoded_block).hexdigest()
+
+def is_chain_valid(chain):
+    # Code goes here
+    previous_block = chain[0]
+    block_index = 1
+
+    while block_index < len(chain):
+
+        block = chain[block_index]
+        if block['previous_hash'] != hash(previous_block):
+            return False
+        
+        previous_nonce = previous_block['nonce']
+        nonce = block['nonce']
+        hash_operation = hashlib.sha256(str(nonce**2 - previous_nonce**2).encode()).hexdigest()
+        
+        if hash_operation[:4] != '0000':
+            return False
+        previous_block = block
+        block_index += 1
+
+    return True
+    
+
+f = open("file2.json", "r")
+chain = json.loads(f.read())
+print(is_chain_valid(chain['chain']))
